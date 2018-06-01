01d77c3e8c296d380f0fa932d01343c336b6455a
707735896 <707735896@qq.com>
2018-06-01 10:51:56 +0800

    修复bug 修改双向数据绑定之间的方法

--- src/components/Translate.vue
+++ src/components/Translate.vue
@@ -2,7 +2,7 @@
   <div>
     <Header></Header>
     <div style="float: left">
-      <textarea v-model="message" placeholder="请输入要翻译的内容"></textarea>
+      <textarea :value="message" @input="updateMessage"></textarea>
       <div class="content">关键字：{{message}}</div>
       <div class="result">{{result}}</div>
     </div>
@@ -17,6 +17,7 @@
 <script>
   import store from '@/vuex/store'
   import Header from '@/components/Header'
+  import {mapState} from 'vuex'
 
   export default {
     name: "Translate",
@@ -43,26 +44,25 @@
     ,
     computed: {
       //处理vuex和v-model之间的双向数据绑定
-      message: {
-        get() {
-          return this.$store.state.message;
-        }
-        ,
-        set(value) {
-          this.$store.commit('updateMessage', value)
-        }
-
-      }
+      ...mapState({
+        message: state => state.message
+      })
     },
     methods: {
       getTrans() {
         let type = this.type;
         let input = this.message;
-        console.log(type, input)
-        this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170721T082515Z.54cf3dc583f679db.f4a96182281281d8b5dfe24b4e88298e2133f219&lang=' + type + '&text=' + input)
-          .then((res) => {
-            this.result = res.data.text.toString();
-          })
+        if (input !== '') {
+          this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170721T082515Z.54cf3dc583f679db.f4a96182281281d8b5dfe24b4e88298e2133f219&lang=' + type + '&text=' + input)
+            .then((res) => {
+              this.result = res.data.text.toString();
+            })
+        } else {
+          return
+        }
+      },
+      updateMessage(e) {
+        this.$store.commit('updateMessage', e.target.value)
       }
     }
     ,
@@ -86,6 +86,7 @@
     width: 500px;
     color: red;
   }
+
   .result {
     border: 1px dashed #71777c;
     color: #007fff;
