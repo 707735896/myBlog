b1636bc518c3e70fe8bfd376e5422282f04596b1
707735896 <707735896@qq.com>
2018-06-19 14:30:13 +0800

    删了没用的文件

--- src/vuex/getters.js
+++
@@ -1,8 +0,0 @@
-// 获取状态信息
-const getters = {
-  username: state => state.login.username,
-  role: state => state.login.role,
-  // newrouter: state => state.login.newrouter,
-  count: (state) => state.count += 100
-}
-export default getters
